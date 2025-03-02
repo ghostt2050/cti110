b3ae81b2755547edf1606ef96b6d58e12af693f6
ghostt2050 <oguziea2083@student.faytechcc.edu>
2025-03-02 14:44:56 -0500

    Create P1HW1_OguzieAmarachi.py

---
+++ P1HW1_OguzieAmarachi.py
@@ -0,0 +1,22 @@
+# Oguzie Amarachi
+# Date: 03/02/2025
+# P1HW1 Instructions
+
+print("-----Calculating Exponents----")
+
+base = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+
+result = base ** exponent
+
+print(f"\n{base} raised to the power of {exponent} is {result} !!")
+
+print("\n-----Addition and Subtraction----")
+
+start_value = int(input("Enter a starting integer: "))
+add_value = int(input("Enter an integer to add: "))
+subtract_value = int(input("Enter an integer to subtract: "))
+
+final_result = start_value + add_value - subtract_value
+
+print(f"\n{start_value} + {add_value} - {subtract_value} is equal to {final_result}")
